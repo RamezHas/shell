174c6c0150a32e8648640bffb2286249f64a7fde
Ramez Hasnaoui <159182949+RamezHas@users.noreply.github.com>
2025-04-20 23:54:48 +0100

    main.c

--- main.c
+++ main.c
@@ -2,55 +2,79 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <sys/wait.h>
-
-
 
 #define MAX_INPUT 1024
 #define MAX_ARGS 64
 
-int main(void) {
-    
+// Built-in function declarations
+void shell_pwd();
+void shell_cd(char *path);
+
+int main(void)
+{
     char input[MAX_INPUT];
-    char *args[MAX_INPUT];
+    char *args[MAX_ARGS];
     char *token;
     int i;
-    pid_t pid;
 
     while (1)
     {
         printf("$ ");
-
-        if(fgets(input, MAX_INPUT, stdin) == NULL) 
+        if (fgets(input, MAX_INPUT, stdin) == NULL)
         {
             printf("\n");
-            break; 
+            break;
         }
 
-        input[strcspn(input, "\n")] = 0; 
-        
+        input[strcspn(input, "\n")] = 0;
+
         token = strtok(input, " ");
         i = 0;
-        while (token != NULL && i < MAX_INPUT-1) {
+        while (token != NULL && i < MAX_ARGS - 1)
+        {
             args[i++] = token;
             token = strtok(NULL, " ");
         }
-        args[i] = NULL; 
+        args[i] = NULL;
 
-        pid = fork();
-        if (pid == 0)
+        if (args[0] == NULL)
+            continue;
+
+        if (strcmp(args[0], "exit") == 0)
         {
-            execvp(args[0],args);
-            perror("execvp");
-            exit(EXIT_FAILURE);
+            break;
         }
-        else if (pid > 0){
-            wait(NULL);
-        }else{
-            perror("fork");
+        else if (strcmp(args[0], "pwd") == 0)
+        {
+            shell_pwd();
+        }
+        else if (strcmp(args[0], "cd") == 0)
+        {
+            shell_cd(args[1]);
+        }
+        else
+        {
+            printf("Command not found: %s\n", args[0]);
         }
     }
-    
 
     return 0;
+}
+
+void shell_pwd()
+{
+    char cwd[1024];
+    if (getcwd(cwd, sizeof(cwd)) != NULL)
+        printf("%s\n", cwd);
+    else
+        perror("pwd");
+}
+
+void shell_cd(char *path)
+{
+    if (path == NULL)
+        path = getenv("HOME");
+
+    if (chdir(path) != 0)
+        perror("cd");
 }
