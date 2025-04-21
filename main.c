8e961782c8ca93127f9f7489ff13301e79ed031e
Ramez Hasnaoui <ramez.hassnaoui@iteam-univ.tn>
2025-04-21 22:59:42 +0100

    Initial commit

--- main.c
+++ main.c
@@ -6,65 +6,61 @@
 #define MAX_INPUT 1024
 #define MAX_ARGS 64
 
-void shell_pwd();
-void shell_cd(char *path);
-void shell_ls();
-void shell_echo(char *args[]);
+typedef void (*cmdFunc)(char **args);
 
-int main(void)
-{
+typedef struct {
+    char *name;
+    cmdFunc func;
+    char *description;
+} cmd;
+
+void shell_exit(char **args);
+void shell_pwd(char **args);
+void shell_echo(char **args);
+void shell_help(char **args);
+
+cmd builtins[] = {
+    {"exit", shell_exit, "Exit the shell"},
+    {"pwd", shell_pwd, "Print current directory"},
+    {"echo", shell_echo, "Print arguments"},
+    {"help", shell_help, "Show this help message"},
+};
+
+int num_builtins = sizeof(builtins) / sizeof(cmd);
+
+int main(void) {
     char input[MAX_INPUT];
     char *args[MAX_ARGS];
-    char *token;
-    int i;
 
-    while (1)
-    {
+    while (1) {
         printf("$ ");
-        if (fgets(input, MAX_INPUT, stdin) == NULL)
-        {
+        if (fgets(input, MAX_INPUT, stdin) == NULL) {
             printf("\n");
             break;
         }
 
         input[strcspn(input, "\n")] = 0;
 
-        // Tokenize input into args
-        token = strtok(input, " ");
-        i = 0;
-        while (token != NULL && i < MAX_ARGS - 1)
-        {
+        int i = 0;
+        char *token = strtok(input, " ");
+        while (token != NULL && i < MAX_ARGS - 1) {
             args[i++] = token;
             token = strtok(NULL, " ");
         }
         args[i] = NULL;
 
-        if (args[0] == NULL)
-            continue;
+        if (args[0] == NULL) continue;
 
-        // Command execution
-        if (strcmp(args[0], "exit") == 0)
-        {
-            break;
+        int found = 0;
+        for (int j = 0; j < num_builtins; j++) {
+            if (strcmp(args[0], builtins[j].name) == 0) {
+                builtins[j].func(args);
+                found = 1;
+                break;
+            }
         }
-        else if (strcmp(args[0], "pwd") == 0)
-        {
-            shell_pwd();
-        }
-        else if (strcmp(args[0], "cd") == 0)
-        {
-            shell_cd(args[1]);
-        }
-        else if (strcmp(args[0], "ls") == 0)
-        {
-            shell_ls();
-        }
-        else if (strcmp(args[0], "echo") == 0)
-        {
-            shell_echo(args);
-        }
-        else
-        {
+
+        if (!found) {
             printf("Command not found: %s\n", args[0]);
         }
     }
@@ -72,9 +68,11 @@ int main(void)
     return 0;
 }
 
-// Built-in: pwd
-void shell_pwd()
-{
+void shell_exit(char **args) {
+    exit(0);
+}
+
+void shell_pwd(char **args) {
     char cwd[1024];
     if (getcwd(cwd, sizeof(cwd)) != NULL)
         printf("%s\n", cwd);
@@ -82,46 +80,20 @@ void shell_pwd()
         perror("pwd");
 }
 
-// Built-in: cd
-void shell_cd(char *path)
-{
-    if (path == NULL)
-        path = getenv("HOME");
-
-    if (chdir(path) != 0)
-        perror("cd");
-}
-
-// Built-in: ls
-void shell_ls()
-{
-    DIR *dir;
-    struct dirent *entry;
-
-    dir = opendir(".");
-    if (dir == NULL)
-    {
-        perror("ls");
-        return;
-    }
-
-    while ((entry = readdir(dir)) != NULL)
-    {
-        if (entry->d_name[0] != '.') 
-            printf("%s  ", entry->d_name);
-    }
-
-    printf("\n");
-    closedir(dir);
-}
-void shell_echo(char *args[]) {
-    if (args[1] == NULL) {
+void shell_echo(char **args) {
+    if (!args[1]) {
         printf("echo: missing operand\n");
         return;
     }
-    while (args[1] != NULL) {
-        printf("%s ", args[1]);
-        args++;
+    for (int i = 1; args[i]; i++) {
+        printf("%s ", args[i]);
     }
     printf("\n");
+}
+
+void shell_help(char **args) {
+    printf("Available commands:\n");
+    for (int i = 0; i < num_builtins; i++) {
+        printf("  %-8s\t%s\n", builtins[i].name, builtins[i].description);
+    }
 }
